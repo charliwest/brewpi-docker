e7d0e7ec3057a5f4f805fd5cc3060eddcbf26e7a
David West <david.c.west@me.com>
2017-09-20 11:10:15 +0200

    Update watcher.sh
    seeing if this will work...

--- ubuntu/watcher.sh
+++ ubuntu/watcher.sh
@@ -3,7 +3,8 @@
 while true
 do
   if ! sudo -u brewpi python /home/brewpi/brewpi.py --checkstartuponly --dontrunfile
-    then sudo -u brewpi python -u /home/brewpi/brewpi.py 1>/home/brewpi/logs/stdout.txt 2>>/home/brewpi/logs/stderr.txt
+    then sudo -u brewpi python -u /home/brewpi/brewpi.py 1>/home/brewpi/logs/stdout.txt 2>>/home/brewpi/logs/stderr.txt &
+         sudo -u brewpi python -u /home/brewpi/iSpindel.py 1>/home/brewpi/logs/isstdout.txt 2>>/home/brewpi/logs/isstderr.txt
   fi
   sleep 30
 done
